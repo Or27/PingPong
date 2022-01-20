e60fd56fbd665082c6a0f5bba126e5bb1a2bef54
Or27 <45974939+Or27@users.noreply.github.com>
2022-01-20 14:35:18 +0200

    Adds class TcpClientSocket

---
+++ Communication/TcpClientSocket.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+using PingPong.Communication.Abstractions;
+using PingPong.Communication.DTOs;
+
+namespace PingPong.Communication
+{
+    public class TcpClientSocket : ClientSocketBase
+    {
+        private TcpClient _wrappedClient;
+
+        public TcpClientSocket(int maxDataSize)
+        {
+            MaxDataSize = maxDataSize;
+            _wrappedClient = new TcpClient();
+        }
+
+        public TcpClientSocket(TcpClient wrappedClient, int maxDataSize)
+        {
+            MaxDataSize = maxDataSize;
+            _wrappedClient = wrappedClient;
+        }
+
+        public override void Connect(ConnectionInfo info)
+        {
+            _wrappedClient.Connect(info.Ip, info.Port);
+        }
+
+        public override byte[] Read()
+        {
+            byte[] receivedData = new byte[MaxDataSize];
+            int bytesAmount = _wrappedClient.GetStream().Read(receivedData, 0, MaxDataSize);
+
+            byte[] compatibleLengthData = new byte[bytesAmount];
+            System.Array.Copy(receivedData, compatibleLengthData, bytesAmount);
+
+            return compatibleLengthData;
+        }
+
+        public override void Write(byte[] data)
+        {
+            _wrappedClient.GetStream().Write(data, 0, data.Length);
+        }
+    }
+}

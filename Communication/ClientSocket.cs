0cf7786d63b606f87229b25e955f5e6c4b7bd6f1
Or27 <45974939+Or27@users.noreply.github.com>
2022-01-20 11:31:01 +0200

    Adds class ClientSocket

---
+++ Communication/ClientSocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using PingPong.Communication.Abstractions;
+using PingPong.Communication.DTOs;
+
+namespace Communication
+{
+    class ClientSocket : ClientSocketBase
+    {
+        private Socket _wrappedClient;
+
+        public ClientSocket()
+        {
+            _wrappedClient = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public override void Connect(ConnectionInfo info)
+        {
+            _wrappedClient.Connect(info.Ip, info.Port);
+        }
+
+        public override byte[] Read()
+        {
+            byte[] receivedData = new byte[100];
+            _wrappedClient.Receive(receivedData);
+
+            return receivedData;
+        }
+
+        public override void Write(byte[] data)
+        {
+            _wrappedClient.Send(data);
+        }
+    }
+}

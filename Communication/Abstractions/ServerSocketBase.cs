94af941d74fc20d128e350f499dda2ca46085c25
Or27 <45974939+Or27@users.noreply.github.com>
2022-01-20 10:22:30 +0200

    Adds abstract class ServerSocketBase

---
+++ Communication/Abstractions/ServerSocketBase.cs
@@ -0,0 +1,9 @@
+﻿
+namespace PingPong.Communication.Abstractions
+{
+    abstract class ServerSocketBase : SocketBase
+    {
+        public abstract ClientSocketBase Accept();
+        public abstract void StartListening(int clientsAmount);
+    }
+}
